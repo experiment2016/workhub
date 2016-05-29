9e06367f03b15575ef043d326fca77f5a0e89a01
experiment2016 <sarpiericam@libero.it>
2016-05-29 21:07:52 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -2,8 +2,8 @@
 def run(**args):
   try:
       print("[*} changing inter")
-      intervallo1 = int(10)
-      intervallo2 = int(50)
+      intervallo1 = int(5)
+      intervallo2 = int(10)
       mess = "int camb"
       return str(mess)
   except Exception, e:
