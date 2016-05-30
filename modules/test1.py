e9ba24dd6c903dd9476c82f08c43094ccadf6b3a
experiment2016 <sarpiericam@libero.it>
2016-05-30 19:20:47 +0200

    Update test1.py

--- modules/test1.py
+++ modules/test1.py
@@ -1,11 +1,8 @@
-import subprocess
+
 
 def run(**args):
   try:
      print("[*] reading")
-     look = "dir"  
-     proc = subprocess.Popen(look, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-     stdoutput = proc.stdout.read() + proc.stderr.read()
-     return str(stdoutput)
+     
   except Exception, e:
       return str(e)
