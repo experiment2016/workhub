ef3f0d2e4d0ae5975f1dbaf10b0972a00992cc24
experiment2016 <sarpiericam@libero.it>
2016-05-30 19:21:14 +0200

    Update test2.py

--- modules/test2.py
+++ modules/test2.py
@@ -1,11 +1,7 @@
-import subprocess
+
 
 def run(**args):
   try:
      print("[*] reading")
-     look = "tasklist"  
-     proc = subprocess.Popen(look, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-     stdoutput = proc.stdout.read() + proc.stderr.read()
-     return str(stdoutput)
   except Exception, e:
       return str(e)
