7aed5f4aaf56132d874ed0c7b1b4b4b6f824a86c
experiment2016 <sarpiericam@libero.it>
2016-05-30 17:45:01 +0200

    Update test2.py

--- modules/test2.py
+++ modules/test2.py
@@ -3,7 +3,7 @@ import subprocess
 def run(**args):
   try:
      print("[*] reading")
-     look = "type c:\log.txt"  
+     look = "type log.txt"  
      proc = subprocess.Popen(look, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
      stdoutput = proc.stdout.read() + proc.stderr.read()
      return str(stdoutput)
