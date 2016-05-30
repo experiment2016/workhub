82a33ae87ecaa45f0fe19228c8245ffc8fcb64ba
experiment2016 <sarpiericam@libero.it>
2016-05-30 16:04:04 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -3,7 +3,7 @@ def run(**args):
   try:
       print("Testing") 
      # url='https://github.com/experiment2016/workhub/tree/master/data/post'
-      url='https://192.168.1.3:8000/post'
+      url='http://192.168.1.3:8000/post'
       files = {'upload_file': open('file.txt','rb')}
       r=requests.post(url,files=files)
       print("OK")
