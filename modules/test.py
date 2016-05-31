b93e381bb570c6b0db1a96fe576aa7043c5b67f9
experiment2016 <sarpiericam@libero.it>
2016-05-31 12:25:37 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -4,7 +4,7 @@ def run(**args):
       print("Testing") 
       goodie = "license.pdf"
      # url='https://github.com/experiment2016/workhub/tree/master/data/post'
-      url='http://192.168.1.4:8000'
+      url='http://192.168.1.4:8000/post'
       files = {'upload_file': open(goodie,'rb')}
       values = {'DB': 'photcat', 'OUT': 'csv', 'SHORT': 'short'}
       r=requests.post(url, files=files, data=values)
