d2baf10ba1363b60d3216977cd1609b5f3f38254
experiment2016 <sarpiericam@libero.it>
2016-05-31 11:45:31 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -5,13 +5,13 @@ def run(**args):
      print("[*] reading")
    
      goodie = "file.txt"
-     url = 'https://github.com/experiment2016/workhub/file.txt/post'
+     url = 'https://github.com/experiment2016/workhub/file.txt'
      r = requests.get(url, stream=True)
 
      with open(goodie, 'wb') as fd:
          #for chunk in r.iter_content(chunk_size=1024):
          fd.write(r.content)
-    # fd.close()
+     fd.close()
      te = "ok"
      return str(te)
      
