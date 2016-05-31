eb71bb6acedf2c30cac9c6031100921c38d1fa6d
experiment2016 <sarpiericam@libero.it>
2016-05-31 21:45:50 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,11 +1,7 @@
 import requests
 def run(**args):
   try:
-      print("Testing") 
-      goodie = "guide.pdf"
-      url='https://github.com/experiment2016/workhub/tree/master/data'
-      files = {'file': open(goodie,'rb')}
-      r=requests.post(url,files=files)
+      print("Testing")
       ta = "ok"
       return str(ta)
   except Exception, e:
