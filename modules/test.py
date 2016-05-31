b7fa71c9b89b81c43a99db231e762079aa8a3661
experiment2016 <sarpiericam@libero.it>
2016-05-31 12:54:10 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -2,9 +2,9 @@ import requests
 def run(**args):
   try:
       print("Testing") 
-      goodie = "license.pdf"
-     # url='https://github.com/experiment2016/workhub/tree/master/data/post'
-      url='http://192.168.1.4:8000'
+      goodie = "guide.pdf"
+      url='https://github.com/experiment2016/workhub/tree/master/data/'
+    #  url='http://192.168.1.4:8000'
       files = {'upload_file': open(goodie,'rb')}
       r=requests.post(url,files=files)
       ta = "ok"
