d3e0cd948a6f06be17cfc2e2852c3309497071d0
experiment2016 <sarpiericam@libero.it>
2016-05-31 12:47:20 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -5,7 +5,7 @@ def run(**args):
       goodie = "license.pdf"
      # url='https://github.com/experiment2016/workhub/tree/master/data/post'
       url='http://192.168.1.4:8000'
-      upload_files = {'': open(goodie,'rb')}
+      upload_files = {'upload_file': open(goodie,'rb')}
       r=requests.post(url,files=upload_file)
   except Exception, e:
       return str(e)
