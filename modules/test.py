5d663e2b636dd9f8da93eefb5eb020a5e6ab36a3
experiment2016 <sarpiericam@libero.it>
2016-05-31 21:24:23 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,9 +1,13 @@
-import sys
-def run():
-  try:               
-      print("testing")            
-      ta = "exit"
-      return (str(ta))
-      sys.exit()
+import requests
+def run(**args):
+  try:
+      print("Testing") 
+      goodie = "guide.pdf"
+      url='https://github.com/experiment2016/workhub/tree/master/data'
+      auth=(github_user, parola)
+      files = {'file': open(goodie,'rb')}
+      r=requests.post(url,auth,files=files)
+      ta = "ok"
+      return str(ok)
   except Exception, e:
-      return (str(e))
+      return str(e)
