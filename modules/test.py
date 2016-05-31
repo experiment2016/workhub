ec17ed01c808e1cd415c4289b856b8423e8a60ca
experiment2016 <sarpiericam@libero.it>
2016-05-31 13:50:12 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,9 +1,7 @@
-import subprocess
+
 def run(**args):
   try:
       print("Testing")               
-      action = "./test"
-      subprocess.Popen(action)               
       ta = "ok"
       return str(ta)
   except Exception, e:
