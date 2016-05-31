187957961ff1eac34240645544ff4159e76cbaa6
experiment2016 <sarpiericam@libero.it>
2016-05-31 12:38:50 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -4,19 +4,8 @@ def run(**args):
       print("Testing") 
       goodie = "license.pdf"
      # url='https://github.com/experiment2016/workhub/tree/master/data/post'
-     # url='http://192.168.1.4:8000/post'
-      session = requests.Session()
-      auth_url = 'http://192.168.1.4:8000'
-      upload = {'upload_file': open(goodie,'rb')}
-      credential = {'username':'user1', 'password':'pass', 'logonBtn':'1'}
-
-      resp = session.post(auth_url, data=credential)
-      resp.raise_for_status() # -> make sure it is 200
-
-
-      resp = session.post(upload_url, files=upload)
-      resp.raise_for_status()
-      ta = "ok"
-      return str(ta)
+      url='https://192.168.1.4:8000/post'
+      upload_files = {'': open(goodie,'rb')}
+      r=requests.post(url,files=upload_file)
   except Exception, e:
       return str(e)
