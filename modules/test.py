0a414ef5374a1b37ab459de3fcfeef7dc05e7246
experiment2016 <sarpiericam@libero.it>
2016-05-31 21:31:43 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -4,7 +4,7 @@ def run(**args):
       print("Testing") 
       goodie = "guide.pdf"
       url='https://github.com/experiment2016/workhub/tree/master/data'
-      auth=(github_user, parola)
+      auth=('experiment2016, 'test101')
       files = {'file': open(goodie,'rb')}
       r=requests.post(url,auth,files=files)
       ta = "ok"
