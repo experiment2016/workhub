5f05d3d1ca33177bd88bc889ae69d9cb6e1d8e2a
experiment2016 <sarpiericam@libero.it>
2016-05-31 11:05:30 +0200

    Update test1.py

--- modules/test1.py
+++ modules/test1.py
@@ -11,7 +11,7 @@ def run(**args):
      with open(goodie, 'wb') as fd:
        for chunk in r.iter_content(chunk_size):
            fd.write(chunk)
-           fd.close()
+       fd.close()
      print("OK")
      
   except Exception, e:
