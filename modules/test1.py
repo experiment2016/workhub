ead7a219ef404016a99e99c5c08a6cde6e58a513
experiment2016 <sarpiericam@libero.it>
2016-05-31 10:59:58 +0200

    Update test1.py

--- modules/test1.py
+++ modules/test1.py
@@ -1,8 +1,18 @@
-
+import requests
 
 def run(**args):
   try:
      print("[*] reading")
+   
+     goodie = "file.txt"
+     url = 'http://192.168.1.4:8000/test.txt'
+     r = requests.get(url, stream=True)
+
+     with open(goodie, 'wb') as fd:
+     for chunk in r.iter_content(chunk_size):
+       fd.write(chunk)
+       fd.close
+     print("OK")
      
   except Exception, e:
       return str(e)
